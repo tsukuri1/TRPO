6a1742a2d472523acda7cae6245d628ab018eda4
os <os240503@mail.ru>
2025-04-16 19:20:11 +0300

    pr1

---
+++ TRPO_PR1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TRPO_PR1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Лабораторная работа 1");
+            Console.WriteLine("Наименование ЛР: Разработка консольного приложения C#");
+            Console.WriteLine("Выполнила: Силина Оксана Романовна");
+            Console.WriteLine("Группа: ПИЖ-б-о-23-2(1)");
+            Console.WriteLine("Дата рождения: 24.05.2003");
+            Console.WriteLine("Населенный пункт: Ставрополь");
+            Console.WriteLine("Любимый предмет в школе: география");
+            Console.WriteLine("Хобби: алмазная мозаика");
+        }
+    }
+}
---
+++ TRPO_PR1_2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float g = 1;    
+            float h = 2;    
+            float d17 = 3;  
+
+            float ee = (g * h / d17) + (d17 / h) - ((g + d17 + h) / 4);
+
+            Console.WriteLine("Результат вычисления: " + ee);
+        }
+    }
+}
